675e244dbc67f7536bfeda04b50b8e34f0a41802
aaronamano <aaroncedric2005@yahoo.com>
2026-01-04 01:13:48 -0500

    prompt template and output parser

---
+++ prompt_template.py
@@ -0,0 +1,25 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+chat_model = ChatOpenAI(openai_api_key=api_key)
+
+template = "You are a helpful assistant taht translates {input_language} to {output_language}."
+human_template = "{text}"
+
+chat_prompt = ChatPromptTemplate.from_messages([
+    ("system", template),
+    ("human", human_template)
+])
+
+messages = chat_prompt.format_messages(input_language="English",
+                                       output_language="French",
+                                       text="I love programming.")
+
+result = chat_model.invoke(messages)
+print(result.content)
